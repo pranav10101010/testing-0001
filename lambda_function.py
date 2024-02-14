950fe765dff8ebecb551f34a600fabafe45438a9
sahilkandwal <135846996+sahilkandwal@users.noreply.github.com>
2024-02-14 16:46:49 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,16 +1,7 @@
 import json
-
 def lambda_handler(event, context):
     # Your main handler logic here
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello sahil')
+        'body': json.dumps('Hello folks')
     }
-{
-#   "LambdaFunctionConfigurations": [
-#     {
-#       "LambdaFunctionArn": "arn:aws:lambda:region:account-id:function:your-lambda-function",
-#       "Events": ["s3:ObjectCreated:*", "s3:ObjectUpdated:*"]
-#     }
-#   ]
-# }

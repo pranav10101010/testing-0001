0440937f2ae273d5570ab5bf3a62fb772612fd0d
pranav10101010 <155971596+pranav10101010@users.noreply.github.com>
2024-03-21 14:34:33 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,5 +3,5 @@ def lambda_handler(event, context):
     # Your main handler logic here
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello bhai ke haal ')
+        'body': json.dumps('Hello bro')
     }

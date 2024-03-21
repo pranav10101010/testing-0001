993054547832e8d3e24724fa01aa44dd0f7b0bd7
pranav10101010 <155971596+pranav10101010@users.noreply.github.com>
2024-03-21 16:22:45 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -6,13 +6,3 @@ def generate_random_number():
 # Example usage
 random_number = generate_random_number()
 print(f'Random number: {random_number}')
-
-
-        # Display the result from the Lambda function on the webpage
-        result_message = data['message']
-        return render_template('result.html', result=result_message)
-    else:
-        return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
